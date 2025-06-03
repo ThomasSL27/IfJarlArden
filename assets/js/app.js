0a88882276cbefa63b05ac47ebac4c014d51a5c6
ThomasSL27 <thomassohus27@gmail.com>
2025-06-03 16:10:42 +0200

    mobil navigation tilføjet til alle sider (HTML, CSS & JS)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
+// Rammer vores burger SVG
+const burger = document.getElementById('burger');
+// Rammer vores nav med id=Menu
+const menu = document.getElementById('menu');
+// Rammer vores lukke-ikon SVG (X)
+const close = document.getElementById('close');
 
+// Definere en function som kan open og lukke
+function burgerMenu(open) {
+    // Hvis "open" === true så tilføjer den klassen toggle.
+    // Hvis den er lukket fjerner den klassen og tilføjer anden klasse
+  menu.classList.toggle('open', open);;
+}
+// Når burgermenuen trykket på, kald så toggleMenu(true)
+burger.addEventListener('click', () => burgerMenu(true));
+// Når lukke ikonet trykkes på, kald så toggleMenu(false)
+close.addEventListener('click', () => burgerMenu(false));
